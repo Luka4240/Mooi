f3c09324206bd3528b65621664508a1773374305
Luke Howard <luke@lukehoward.me.uk>
2013-07-17 14:01:16 +0100

    New Files

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mooi | A Beautifully Clean and Simple SCSS Framework</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href='http://fonts.googleapis.com/css?family=Arimo|Roboto' rel='stylesheet' type='text/css'>
+<link href="stylesheets/style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<header class="row mooi-header">
+	<div class="five columns">
+		<img src="images/mooi_logo.png" alt="Mooi" width="300" height="114" class="logo" />
+	</div>
+    <div class="eleven columns">
+    <p>A Beautifully Clean and Simple SCSS Framework.</p>
+    </div>
+</header>    
+
+<div class="row">
+	<div class="sixteen columns">
+    	<p>Mooi is a collection of SCSS files that will allow you to develop a website that will look great no matter what the screen size. It was built to speed up the development of any website, be it a wireframe site, or a fully function corporate site.</p>
+    	<p>Mooi was designed and created by Luke Howard and Chris Brett, by using a collection of experience and tools available to them.</p>
+    </div>
+</div>
+
+
+
+
+
+<div class="row">
+	<div class="sixteen columns">
+    	<h3>The Grid</h3>
+		<p>Our Grid system is based on the 960 grid system except it is varied to run on higher resolutions.
+    </div>
+    
+    
+  <div class="one columns example-col">One</div>
+  <div class="fifteen columns example-col">Fifteen</div>
+    
+  <div class="two columns example-col">Two</div>
+  <div class="fourteen columns example-col">Fourteen</div>
+    
+    <div class="three columns example-col">Three</div>
+    <div class="thirteen columns example-col">Thirteen</div>
+    
+    <div class="four columns example-col">Four</div>
+    <div class="twelve columns example-col">Twelve</div>
+    
+    <div class="five columns example-col">Five</div>
+    <div class="eleven columns example-col">Eleven</div>
+    
+    <div class="six columns example-col">Six</div>
+    <div class="ten columns example-col">Ten</div>
+    
+    <div class="seven columns example-col">Seven</div>
+    <div class="nine columns example-col">Nine</div>
+    
+    <div class="eight columns example-col">Eight</div>
+    <div class="eight columns example-col">Eight</div>
+    
+    <div class="nine columns example-col">Nine</div>
+    <div class="seven columns example-col">Seven</div>
+    
+    <div class="ten columns example-col">Ten</div>
+    <div class="six columns example-col">Six</div>
+    
+    <div class="eleven columns example-col">Eleven</div>
+    <div class="five columns example-col">Five</div>
+    
+    <div class="twelve columns example-col">Twelve</div>
+    <div class="four columns example-col">Four</div>
+    
+    <div class="thirteen columns example-col">Thirteen</div>
+    <div class="three columns example-col">Three</div>
+    
+    <div class="fourteen columns example-col">Fourteen</div>
+    <div class="two columns example-col">Two</div>
+    
+    <div class="fifteen columns example-col">Fifteen</div>
+    <div class="one columns example-col">One</div>
+    
+  <div class="sixteen columns example-col">Sixteen</div>
+    
+    
+</div>
+
+
+</body>
+</html>
