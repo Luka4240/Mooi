1379ab7607a807c5d7f44b03e29a2ea3933208d1
Luke Howard <luke@lukehoward.me.uk>
2013-07-22 15:13:18 +0100

    V0.06
    Buttons Complete, Typo Complete

--- index.php
+++ index.php
@@ -115,6 +115,46 @@
     <div class="two-thirds column example-col">Two Thirds</div>
     <div class="one-third column example-col">One Third</div>
 </div>
+
+<div class="row section">
+	<div class="sixteen columns">
+    	<h2>Typography</h2>
+        <p>The typography for Mooi is set up so that it is easy to read no matter what screen size. Regular font paragraphs are set to 16px with a line height of 24px (1rem), these can be easily changed by modifying the _vars.scss file. </p>
+	</div>
+    <div class="eight columns"> 
+    	<h5>Heading</h5>       
+        <h1>Heading &lt;h1&gt;</h1>
+        <h2>Heading &lt;h2&gt;</h2>
+        <h3>Heading &lt;h3&gt;</h3>
+        <h4>Heading &lt;h4&gt;</h4>
+        <h5>Heading &lt;h5&gt;</h5>
+        <h6>Heading &lt;h6&gt;</h6>
+	</div>
+    <div class="eight columns">  
+    	<h5>Block Quote</h5>      
+        <blockquote>"I wanted Mooi to be a framework that speeded up development for me, and made my life easier when working on a site. For that reason I thought others should have access to it as well" - Luke Howard</blockquote>
+        <h5>Quote</h5>
+        <q>"I had great fun developing Mooi" - Luke Howard</q>
+        
+        <h5>Paragraph</h5>
+        <p>This is an example of a default paragraph within Mooi. It is easy to modify the paragraph so that it looks the way you want.</p>
+        
+	</div>
+    
+    <div class="sixteen columns">
+    
+    	<p class="code">
+        &lt;h1&gt;Heading&lt;/h1&gt; <br/>
+        &lt;p&gt;This is a paragraph&lt;/p&gt;<br/>
+        &lt;blockquote&gt;This is a blockquote&lt;/blockquote&gt;<br/>
+        &lt;q&gt;This is a quote&lt;/q&gt;
+             
+        
+        </p>
+    
+    </div>
+    
+</div>            
    
 <div class="row  section">
 	<div class="sixteen columns">
@@ -126,12 +166,12 @@
         <p>To use the Mooi style buttons by adding <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
         
         <a href="#" class="mooi-button">Default Button</a>
-        <button class="mooi-button"/>Default Button</button>
+        <button class="mooi-button">Default Button</button>
        
         
         <p class="code">
         &lt;a href="#" class="mooi-button"&gt;Default Button&lt;/a&gt;<br/>
-        &lt;button class="mooi-button"/&gt;Default Button&lt;/button&gt;
+        &lt;button class="mooi-button"&gt;Default Button&lt;/button&gt;
         </p>
         
         <h5>Disabled Button</h5>
@@ -139,11 +179,11 @@
         <p>To disable a button add <code>mooi-button-disabled</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
         
         <a href="#" class="mooi-button mooi-button-disabled">Disabled Button</a>
-        <button class="mooi-button mooi-button-disabled"/>Disabled Button</button>
+        <button class="mooi-button mooi-button-disabled">Disabled Button</button>
         
         <p class="code">
         &lt;a href="#" class="mooi-button mooi-button-disabled"&gt;Disabled Button&lt;/a&gt;<br/>
-        &lt;button class="mooi-button mooi-button-disabled"/&gt;Disabled Button&lt;/button&gt;
+        &lt;button class="mooi-button mooi-button-disabled"&gt;Disabled Button&lt;/button&gt;
         </p>
         
         <h5>Primary Button</h5>
@@ -151,52 +191,95 @@
         <p>To use the primary button add <code>mooi-button-primary</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
         
         <a href="#" class="mooi-button mooi-button-primary">Primary Button</a>
-        <button class="mooi-button mooi-button-primary"/>Primary Button</button>
+        <button class="mooi-button mooi-button-primary">Primary Button</button>
         
         <p class="code">
         &lt;a href="#" class="mooi-button mooi-button-primary"&gt;Primary Button&lt;/a&gt;<br/>
-        &lt;button class="mooi-button mooi-button-primary"/&gt;Primary Button&lt;/button&gt;
+        &lt;button class="mooi-button mooi-button-primary"&gt;Primary Button&lt;/button&gt;
         </p>
         
 		<h5>Coloured Button</h5>
         
         <p>To use on of the coloured buttons add <code>mooi-button-red</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
         
-        <button class="mooi-button mooi-button-green"/>Green Button</button>
-        <button class="mooi-button mooi-button-red"/>Red Button</button>
-        <button class="mooi-button mooi-button-orange"/>Orange Button</button>
-        <button class="mooi-button mooi-button-blue"/>Blue Button</button>
+        <button class="mooi-button mooi-button-green">Green Button</button>
+        <button class="mooi-button mooi-button-red">Red Button</button>
+        <button class="mooi-button mooi-button-orange">Orange Button</button>
+        <button class="mooi-button mooi-button-blue">Blue Button</button>
         
         <p class="code">
-        &lt;button class="mooi-button mooi-button-green"/&gt;Green Button&lt;/button&gt;<br/>
-        &lt;button class="mooi-button mooi-button-red"/&gt;Red Button&lt;/button&gt;<br/>
-        &lt;button class="mooi-button mooi-button-orange"/&gt;Orange Button&lt;/button&gt;<br/>
-        &lt;button class="mooi-button mooi-button-blue"/&gt;Blue Button&lt;/button&gt;
+        &lt;button class="mooi-button mooi-button-green"&gt;Green Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-red"&gt;Red Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-orange"&gt;Orange Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-blue"&gt;Blue Button&lt;/button&gt;
         </p>
         
         <h5>Different Sized Buttons</h5>
         
         <p>To change the size of the buttons add <code>mooi-button-small</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
         
-        <button class="mooi-button mooi-button-xsmall"/>X-Small Button</button>
-        <button class="mooi-button mooi-button-small"/>Small Button</button>
-        <button class="mooi-button"/>Regular Button</button>
-        <button class="mooi-button mooi-button-large"/>Large Button</button>
-        <button class="mooi-button mooi-button-xlarge"/>X-Large Button</button>
+        <button class="mooi-button mooi-button-xsmall">X-Small Button</button>
+        <button class="mooi-button mooi-button-small">Small Button</button>
+        <button class="mooi-button">Regular Button</button>
+        <button class="mooi-button mooi-button-large">Large Button</button>
+        <button class="mooi-button mooi-button-xlarge">X-Large Button</button>
         
 		<p class="code">
-        &lt;button class="mooi-button mooi-button-xsmall"/&gt;X-Small Button&lt;/button&gt;<br/>
-        &lt;button class="mooi-button mooi-button-small"/&gt;Small Button&lt;/button&gt;<br/>
-        &lt;button class="mooi-button"/&gt;Regular Button&lt;/button&gt;<br/>
-        &lt;button class="mooi-button mooi-button-large"/&gt;Large Button&lt;/button&gt;<br/>
-        &lt;button class="mooi-button mooi-button-xlarge"/&gt;X-Large Button&lt;/button&gt;
+        &lt;button class="mooi-button mooi-button-xsmall"&gt;X-Small Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-small"&gt;Small Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button"&gt;Regular Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-large"&gt;Large Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-xlarge"&gt;X-Large Button&lt;/button&gt;
         </p>
         
     </div>
 </div>    
 
+<div class="row  section">
+	<div class="sixteen columns">
+    	<h2>Forms</h2>
+        
+        <p></p>
+        <label>Regular Input</label>
+        <input type="text" />
+    	<label>Regular Textarea</label>
+    	<textarea></textarea>
+        <label>Select List</label>
+        <select>
+        	<option value="Option 1">Option 1</option>
+            <option value="Option 2">Option 2</option>
+            <option value="Option 3">Option 3</option>
+		</select>            
+        
+        <label>Regular Checkbox</label>
+        <input type="checkbox" />
+        <input type="checkbox" checked />
+        <label>Regular Radio Button</label>
+        <input type="radio" />
+        <input type="radio" checked />
+        <label>Regular Button</label>
+        <input type="button" class="mooi-button" value="Submit" />
+    </div>
+</div>
 
+<div class="row section">
+	<div class="sixteen columns">
+    <h2>The License</h2>
+    	
+        <p>Every part of Mooi is free to use, and if you want to tear it apart, change it, or just use it as it was designed feel free. The full license for Mooi is just below, and can be found here as well <a href="http://opensource.org/licenses/mit-license.php">http://opensource.org/licenses/mit-license.php</a>.
+    
+		<h5>The MIT License (MIT)</h5>
 
+		<h5>Copyright &copy; 2013 Luke Howard.</h5>
+
+		<p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+
+		<p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+
+		<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+        
+	</div>
+</div>
 
 <footer class="row">
     <div class="one-third column">
