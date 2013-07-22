942a1d3e78b8e0eeba8520fa891dd4089a47b05f
Luke Howard <luke@lukehoward.me.uk>
2013-07-22 13:43:24 +0100

    V0.05
    Buttons Completed

--- index.php
+++ index.php
@@ -35,36 +35,30 @@
 	</div>        
 </div>
 
-<div class="row">
+<div class="row  section">
 	<div class="sixteen columns">
     	<p class="intro">Mooi is a SCSS Framework designed to speed up development times, with Mooi you can wireframe a site in a matter of minutes by setting a few variables and then adding your own CSS to the StyleSheets.</p>
         <p class="intro">Mooi has been designed, developed and created with users in mind, the concept is that nothing should be overly complicated and that by using this framework it will reduce the lines of code that you have to write, and let Mooi do the rest for you.</p>
     </div>
 </div>
-<div class="row">
-	<div class="sixteen columns">
-    	<img class="image-center  scroll" src="images/scrolldown.png" width="300px" height="83">
-	</div>
-</div>            
+   
 
-
-<div class="row">
+<div class="row  section">
 	<div class="sixteen columns">
-    	<input type="button" class="mooi-button" value="Button" />
-        <input type="button" class="mooi-button mooi-button-disabled" value="Button" />
-        <input type="button" class="mooi-button mooi-button-primary" value="Button" />
-        <input type="button" class="mooi-button mooi-button-success" value="Button" />
-        <input type="button" class="mooi-button mooi-button-error" value="Button" />
-        <input type="button" class="mooi-button mooi-button-warning" value="Button" />
-        <input type="button" class="mooi-button mooi-button-secondary" value="Button" />
+    	<h3>The Layout</h3>
+		<p>The Layout is based on Skeleton, but uses a fluid design with break points to ensure a responsive design, so that no matter what device views a site using Mooi, it looks great each and every time.</p>
     </div>
-</div>    
-
-
-<div class="row">
-	<div class="sixteen columns">
-    	<h3>The Grid</h3>
-		<p>Our Grid system is based on the <a href="#">960 grid system </a>except it is varied to run on higher resolutions.
+    
+    <div class="sixteen columns">
+    
+        <p class="code">
+			&lt;div class="row"&gt;<br/>
+            &lt;div class="sixteen columns"&gt;<br/>
+            &lt;p&gt;This is an example element&lt;/p&gt;<br/>
+            &lt;/div&gt;<br/>
+            &lt;/div&gt;
+        
+        </p>
     </div>
     
   <div class="one columns example-col">One</div>
@@ -121,6 +115,87 @@
     <div class="two-thirds column example-col">Two Thirds</div>
     <div class="one-third column example-col">One Third</div>
 </div>
+   
+<div class="row  section">
+	<div class="sixteen columns">
+    
+    <h2>The Buttons</h2>
+    	
+        <h5>Default Button</h5>
+        
+        <p>To use the Mooi style buttons by adding <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
+        
+        <a href="#" class="mooi-button">Default Button</a>
+        <button class="mooi-button"/>Default Button</button>
+       
+        
+        <p class="code">
+        &lt;a href="#" class="mooi-button"&gt;Default Button&lt;/a&gt;<br/>
+        &lt;button class="mooi-button"/&gt;Default Button&lt;/button&gt;
+        </p>
+        
+        <h5>Disabled Button</h5>
+        
+        <p>To disable a button add <code>mooi-button-disabled</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
+        
+        <a href="#" class="mooi-button mooi-button-disabled">Disabled Button</a>
+        <button class="mooi-button mooi-button-disabled"/>Disabled Button</button>
+        
+        <p class="code">
+        &lt;a href="#" class="mooi-button mooi-button-disabled"&gt;Disabled Button&lt;/a&gt;<br/>
+        &lt;button class="mooi-button mooi-button-disabled"/&gt;Disabled Button&lt;/button&gt;
+        </p>
+        
+        <h5>Primary Button</h5>
+        
+        <p>To use the primary button add <code>mooi-button-primary</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
+        
+        <a href="#" class="mooi-button mooi-button-primary">Primary Button</a>
+        <button class="mooi-button mooi-button-primary"/>Primary Button</button>
+        
+        <p class="code">
+        &lt;a href="#" class="mooi-button mooi-button-primary"&gt;Primary Button&lt;/a&gt;<br/>
+        &lt;button class="mooi-button mooi-button-primary"/&gt;Primary Button&lt;/button&gt;
+        </p>
+        
+		<h5>Coloured Button</h5>
+        
+        <p>To use on of the coloured buttons add <code>mooi-button-red</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
+        
+        <button class="mooi-button mooi-button-green"/>Green Button</button>
+        <button class="mooi-button mooi-button-red"/>Red Button</button>
+        <button class="mooi-button mooi-button-orange"/>Orange Button</button>
+        <button class="mooi-button mooi-button-blue"/>Blue Button</button>
+        
+        <p class="code">
+        &lt;button class="mooi-button mooi-button-green"/&gt;Green Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-red"/&gt;Red Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-orange"/&gt;Orange Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-blue"/&gt;Blue Button&lt;/button&gt;
+        </p>
+        
+        <h5>Different Sized Buttons</h5>
+        
+        <p>To change the size of the buttons add <code>mooi-button-small</code> after the <code>mooi-button</code> class to any <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.</p>
+        
+        <button class="mooi-button mooi-button-xsmall"/>X-Small Button</button>
+        <button class="mooi-button mooi-button-small"/>Small Button</button>
+        <button class="mooi-button"/>Regular Button</button>
+        <button class="mooi-button mooi-button-large"/>Large Button</button>
+        <button class="mooi-button mooi-button-xlarge"/>X-Large Button</button>
+        
+		<p class="code">
+        &lt;button class="mooi-button mooi-button-xsmall"/&gt;X-Small Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-small"/&gt;Small Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button"/&gt;Regular Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-large"/&gt;Large Button&lt;/button&gt;<br/>
+        &lt;button class="mooi-button mooi-button-xlarge"/&gt;X-Large Button&lt;/button&gt;
+        </p>
+        
+    </div>
+</div>    
+
+
 
 
 <footer class="row">
